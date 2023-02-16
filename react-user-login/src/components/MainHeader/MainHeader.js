bc2bba910a4a9f197bfb5e2a1afd35f50de0fa45
Jyddys <justus.sydanmaa@hotmail.com>
2023-02-16 09:23:47 +0200

    test change

--- react-user-login/src/components/MainHeader/MainHeader.js
+++ react-user-login/src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      <h1>My page</h1>
       <Navigation onLogout={props.onLogout} />
     </header>
   );
